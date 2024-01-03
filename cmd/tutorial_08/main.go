96f51f7de0606fc909558145f8c0dd8798894267
Stephen Newman <steve@newman.digital>
2024-01-03 08:50:12 +0000

    Goroutines

---
+++ cmd/tutorial_08/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var m = sync.RWMutex{}
+var wg = sync.WaitGroup{}
+var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
+var results = []string{}
+
+func main() {
+	t0 := time.Now()
+	for i := 0; i < len(dbData); i++ {
+		wg.Add(1)
+		go dbCall(i)
+	}
+
+	wg.Wait()
+	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
+	fmt.Printf("\nThe results are %v", results)
+}
+
+func dbCall(i int) {
+	// Simulate DB call delay
+	var delayDuration float32 = 2000
+	time.Sleep(time.Duration(delayDuration) * time.Millisecond)
+	fmt.Println("The result from the database is: ", dbData[i])
+	save(dbData[i])
+	log()
+	wg.Done()
+}
+
+func save(result string) {
+	m.Lock()
+	results = append(results, result)
+	m.Unlock()
+}
+
+func log() {
+	m.RLock()
+	fmt.Printf("\nThe results are %v", results)
+	m.RUnlock()
+}
